f8bb9756749e64acaa1177c71f1573a9fd2b4b00
David Gullmarsvik <awt2.2012@gmail.com>
2012-09-13 08:30:25 +0200

    Buggfix Uppgift 6

--- Lektion12.Web/ViewModels/ProductEditViewModel.cs
+++ Lektion12.Web/ViewModels/ProductEditViewModel.cs
@@ -9,13 +9,10 @@ namespace Lektion12.Web.ViewModels
 {
     public class ProductEditViewModel
     {
-        public class ProductEditViewModel
-        {
-            public Product Product { get; set; }
-            public IEnumerable<System.Web.Mvc.SelectListItem> Categories { get; set; }
-            public IEnumerable<System.Web.Mvc.SelectListItem> Subcategories { get; set; }
-            [DisplayName("Select a Subcategory")]
-            public int SelectedID { get; set; }
-        }
+        public Product Product { get; set; }
+        public IEnumerable<System.Web.Mvc.SelectListItem> Categories { get; set; }
+        public IEnumerable<System.Web.Mvc.SelectListItem> Subcategories { get; set; }
+        [DisplayName("Select a Subcategory")]
+        public int SelectedID { get; set; }
     }
 }
