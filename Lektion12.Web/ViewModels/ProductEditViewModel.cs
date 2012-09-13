b79bd8410d2c3bd401a1a9b99f40a5a15d0d351b
David Gullmarsvik <awt2.2012@gmail.com>
2012-09-13 08:28:57 +0200

    Uppgift 6

---
+++ Lektion12.Web/ViewModels/ProductEditViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lektion12.Data.Entities;
+using System.ComponentModel;
+
+namespace Lektion12.Web.ViewModels
+{
+    public class ProductEditViewModel
+    {
+        public class ProductEditViewModel
+        {
+            public Product Product { get; set; }
+            public IEnumerable<System.Web.Mvc.SelectListItem> Categories { get; set; }
+            public IEnumerable<System.Web.Mvc.SelectListItem> Subcategories { get; set; }
+            [DisplayName("Select a Subcategory")]
+            public int SelectedID { get; set; }
+        }
+    }
+}
