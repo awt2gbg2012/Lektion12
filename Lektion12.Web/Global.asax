b1ff44e52cbc8cbc8c9a45765d85295a2bf7d15c
David Gullmarsvik <awt2.2012@gmail.com>
2012-09-13 08:01:46 +0200

    Innan Övningar

---
+++ Lektion12.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lektion12.Web.MvcApplication" Language="C#" %>
---
+++ Lektion12.Web/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lektion12.Data.Entities;
+using Lektion12.Web.ViewModels;
+
+namespace Lektion12.Web.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string SearchString { get; set; }
+    }
+}
